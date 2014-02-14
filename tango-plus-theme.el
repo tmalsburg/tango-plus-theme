53fea2ddaf26e31f459498f8fc942d188d0e0403
Titus von der Malsburg <malsburg@posteo.de>
2014-02-14 10:20:56 +0000

    Corrected header of commentary section.

--- tango-plus-theme.el
+++ tango-plus-theme.el
@@ -20,7 +20,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary
+;;; Commentary:
 
 ;; This theme is based on the tango theme that is part of GNU Emacs.
 ;;
